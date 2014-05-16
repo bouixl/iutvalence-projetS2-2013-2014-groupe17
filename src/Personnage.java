3ac3738b325e976498107ed844af1cbf8761d790
rougen <rougen@PC-B124-05.iut-valence.fr>
2014-05-16 08:48:28 +0200

    création de la classe personnage

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,5 +1,93 @@
-
+/**
+ * Classe qui crée un personnage
+ * Un personnage dans le jeu possède un nom unique un niveau avec de l'expérience 
+ * Un personnage appartient à une classe, a des statistiques(force intelligence agilité hp mp ...)
+ * 
+ * @author rougen
+ *
+ */
 public class Personnage
 {
-
+	/**
+	 * le nom ou pseudo du personnage creer
+	 */
+ 	public String nomPersonnage;
+	 
+	/**
+	 * le niveau actuel du personnage
+	 * une montée de niveau augmente les statistiques et peut débloquer des sorts
+	 */
+	public int niveauPersonnage;
+	
+	/**
+	 * l'expérience du personnage
+	 * gagner de l'expérience permet de monter de niveau
+	 * on gagne de l'expérience en battant des ennemis et en finissant des quêtes 
+	 */
+	private int experiencePersonnage;
+	
+	/**
+	 * La classe du personnage
+	 * Une classe donne accès à des sorts uniques de classes et des statistiques plus élevé pour cette classe
+	 * un personnage peut être barbare magicien prêtre ...(a rajouter)
+	 */
+	public String classePersonnage;
+	
+	/**
+	 *un personnage peut avoir de l'équipement 
+	 *On equipe la  tete, le corps, les jambes et les armes(mains) 
+	 */
+	
+	public Item[] equipementPersonnage;
+	
+	/**
+	 * la force du personnage définit ses dégats physiques avec ses armes ou ses sorts physiques
+	 */
+	private int force;
+	/**
+	 * La constitution du personnage définit la défense physique et magique du personnage aux dégats
+	 * Augmente aussi les points de vie du personnage
+	 */
+	private int constitution;
+	/**
+	 * La dextérité du personnage définit les chances d'esquives, de toucher et de critiques du personnage
+	 */
+	private int dextérité;
+	/**
+	 * L'intelligence du personnage définit la puissance des sorts magiques et augmente les points de mana du personnage
+	 */
+	private int intelligence;
+	/**
+	 * Les points de vies actuels du personnage
+	 * Si ils tombent à zéro, le personnage meure
+	 */
+	public int pointsDeVie;
+	/**
+	 * les points de vies maximum d'un personnage
+	 * Ces points de vies augmentent à chaque niveau
+	 */
+	public int pointsDeVieMax;
+	/**
+	 * Les points de mana actuels du personnage
+	 * on utilise des points de mana pour chaque utilisation de compétence
+	 * si les points de mana sont plus bas que le cout nécessaire d'utilisation d'une compétence, on ne peut utiliser la compétence
+	 */
+	public int pointsDeMana;
+	
+	public int pointsDeManaMax;
+	/**
+	 * constructeur qui va crée un personnage en demandant un nom et une classe dans une liste au Joueur
+	 * Le nouveau personnage sera au niveau 1 et aura 0 expérience de base
+	 */
+	public Personnage()
+	{
+		this.nomPersonnage = "Roger" ; 
+		this.niveauPersonnage = 1;
+		this.experiencePersonnage = 0 ;
+		this.classePersonnage = "Barbare" ;
+		this.force=10;
+		this.constitution=
+		this.intelligence=
+	}
+	
 }
