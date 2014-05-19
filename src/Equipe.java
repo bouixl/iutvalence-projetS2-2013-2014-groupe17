44d8806a4f78185012c5577ec95ce54c5380197a
sanfilma <sanfilma@PC-B124-04.iut-valence.fr>
2014-05-19 15:33:32 +0200

    Debut implementation equipe par Max

---
+++ src/Equipe.java
@@ -0,0 +1,37 @@
+
+public class Equipe
+{
+	
+	private int X;
+	private int Y;
+	private int id_map;
+	private int Orientation;
+	
+	public int getX()
+	{
+		return this.X;
+	}
+
+	public int getY()
+	{
+		return this.Y;
+	}
+
+	public int getid_map()
+	{
+		return this.id_map;
+	}
+	
+	public int getOrientation()
+	{
+		return this.Orientation;
+	}
+	
+	public void ajoutpersonnage(Personnage)
+	{
+		
+	}
+	
+	
+}
+
