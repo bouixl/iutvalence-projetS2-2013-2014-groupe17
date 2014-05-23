e14ef29318e0b8cc7c7fb23944b7d6d0c060afb7
Boris <Boris@Sirob>
2014-05-23 11:51:12 +0200

    Génération des items par Boris

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,19 @@
+import java.util.Random;
+
 public class Application
 {
 	/**
 	 * Methode principale, initialise le programme et lance la partie
 	 * @param args Arguments de lancement
 	 */
+	public static Random RNG = new Random();
+	
 	public static void main(String[] args)
 	{
+		for(int i = 0;i<100;i++)
+		{
+			System.out.println(Item.genererEquipement(1));
+		}
 		System.out.println("Création de la partie");
 		Partie partie = new Partie();
 		System.out.println("Demarrage de la partie");
--- src/Item.java
+++ src/Item.java
@@ -5,5 +5,79 @@
  */
 public class Item
 {
+	private final int type;
+	private String nom;
+	private int[] effet;
+	
+	private static final String[] TYPES_ARMES={"Epée","Hache","Poêle à frire","Hallebarde","Katana","Lance","Arbalête","Batte", "Gourdin", "Masse","Arc","Marteau","Espadon","Bâton" };
+	private static final String[] TYPES_ARMURES={"Côte de mailles","Armure","Robe","Plastron"};
+	private static final String[] ADJECTIFS_POSITIFS={"Démonique","Brutal","Enchanté","Mystique","Aiguisé","Légendaire","de Bonne Qualité","Luminescente"};
+	private static final String[] ADJECTIFS_NEGATIFS={"Emoussé","Tordu","Rouillé","Abimé","de Débutant","de Mauvaise Qualité", "Cassé"};
+	private static final String[] MATERIAUX={"en Fer","en Cuivre","en Or","en Cuir","en Tissu","en Mithril","en Adamantium","en Cobalt","en Bois"};
+	
+	public Item(int type, String nom, int[] effet) {
+		this.type = type;
+		this.nom = nom;
+		this.effet = effet;
+	}
 
+
+	/**
+	 * @return the type
+	 */
+	public int obtenirType() {
+		return this.type;
+	}
+
+
+	/**
+	 * @return the nom
+	 */
+	public String obtenirNom() {
+		return this.nom;
+	}
+
+
+	/**
+	 * @return the effet
+	 */
+	public int[] obtenirEffet() {
+		return this.effet;
+	}
+	
+	public static String genererEquipement(int niveau){
+		String nomProduit;
+		String[] categorie;
+		String[] adjectifs;
+		int id;
+		
+		int type = Application.RNG.nextInt(2);
+		
+		if (type==1)
+			categorie=TYPES_ARMES;
+		else
+			categorie=TYPES_ARMURES;
+		
+		id=Application.RNG.nextInt(categorie.length);
+		nomProduit=" "+categorie[id];
+		
+		id=Application.RNG.nextInt(MATERIAUX.length);
+		nomProduit+=" "+MATERIAUX[id];
+		
+		if (Application.RNG.nextBoolean())
+		{
+			adjectifs=ADJECTIFS_POSITIFS;
+		}
+		else
+		{
+			adjectifs=ADJECTIFS_NEGATIFS;
+		}
+		
+		id=Application.RNG.nextInt(adjectifs.length);
+		nomProduit+=" "+adjectifs[id];
+		
+		return nomProduit;
+	}
+
+	
 }
